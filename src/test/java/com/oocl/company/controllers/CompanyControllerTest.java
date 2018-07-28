c4cbbbd9abb200ccaea6401522c0ab32463675c7
Terry Teng <terry.teng@oocl.com>
2018-07-28 19:35:33 +0800

    [Terry] CompanyControllerTest get companyDTO list when call get all companies test done

--- src/test/java/com/oocl/company/controllers/CompanyControllerTest.java
+++ src/test/java/com/oocl/company/controllers/CompanyControllerTest.java
@@ -1,6 +1,7 @@
 package com.oocl.company.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oocl.company.controllers.DTO.CompanyDTO;
 import com.oocl.company.entities.Company;
 import com.oocl.company.service.CompanyService;
 import org.junit.Test;
@@ -8,19 +9,28 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CompanyController.class)
+@EnableSpringDataWebSupport
 public class CompanyControllerTest {
     @Autowired
     private ObjectMapper mapper;
@@ -42,6 +52,22 @@ public class CompanyControllerTest {
                                                             .content(mapper.writeValueAsString(company)))
                 .andExpect(status().isCreated())
                 .andDo(print());
+    }
+    
+    @Test
+    public void should_return_companyDTO_list_when_get_all_company() throws Exception{
+    // given
+        Company company = new Company(1L, "oocl");
+        CompanyDTO companyDTO = new CompanyDTO(company);
+        List<CompanyDTO> companies = new ArrayList<>();
+        companies.add(companyDTO);
+        given(companyService.getAllCompany(any())).willReturn(companies);
+    // when
+    // then
+        mockMvc.perform(get("/api/v1/companies").contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].name", is("oocl")));
     }
 
 }
